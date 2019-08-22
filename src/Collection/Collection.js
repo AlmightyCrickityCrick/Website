1309f6bdc0c2406369ecdfb49e35c9c14fb696b1
Lina Scripca <lscripca@Ellations-iMac.local>
2019-08-22 10:10:33 +0300

    Made changes in collection

---
+++ src/Collection/Collection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../Card/Card";
+
+function Collection() {
+  return (
+    <div className="collection">
+      <Card></Card>
+    </div>
+  );
+}
+
+export default Collection;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Collection from "../Collection/Collection";
 
 function Home() {
-  return <Link to="about">Go To About Page</Link>;
+  return (
+    <Link to="about">Go To About Page</Link>,
+    (
+      <div>
+        <Collection> </Collection>
+        <Collection></Collection>
+        <Collection></Collection>
+      </div>
+    )
+  );
 }
 
 export default Home;
